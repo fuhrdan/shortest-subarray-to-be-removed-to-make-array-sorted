454447f7129ca4d60dc8693c72446fd123a388f4
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-11-15 07:13:32 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿//*****************************************************************************
+//** 1574. Shortest Subarray to be Removed to Make Array Sorted    leetcode  **
+//*****************************************************************************
+
+int findLengthOfShortestSubarray(int* arr, int arrSize)
+{
+    int l = 0, r = arrSize - 1;
+
+    while (l + 1 < arrSize && arr[l] <= arr[l + 1])
+    {
+        l++;
+    }
+
+    if (l == arrSize - 1)
+    {
+        return 0;
+    }
+
+    while (r - 1 > 0 && arr[r - 1] <= arr[r])
+    {
+        r--;
+    }
+
+    int min_len = (arrSize - 1 - l < r) ? arrSize - 1 - l : r;
+
+    for (int i = 0, j = r; i <= l && j < arrSize; ++i)
+    {
+        while (j < arrSize && arr[i] > arr[j])
+        {
+            j++;
+        }
+
+        if (j < arrSize)
+        {
+            int current_len = j - 1 - i;
+            if (current_len < min_len)
+            {
+                min_len = current_len;
+            }
+        }
+    }
+
+    return min_len;
+}
